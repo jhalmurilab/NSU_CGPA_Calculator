32efae33fc751437f16bfe4a8d519d11d2760ba0
jhalmurilab <jhalmurilab@gmail.com>
2015-07-12 22:43:56 +0600

    Add Course  Grade list To  <string-array name="planets_array">

--- app/src/main/java/com/jhalmurilab/nsucgpacalculator/AddCourse.java
+++ app/src/main/java/com/jhalmurilab/nsucgpacalculator/AddCourse.java
@@ -1,17 +1,40 @@
 package com.jhalmurilab.nsucgpacalculator;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
-public class AddCourse extends ActionBarActivity {
+public class AddCourse extends Activity {
 
+    Spinner spinner;
+    public ArrayAdapter<CharSequence> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_course);
+        spinner =(Spinner) findViewById(R.id.planets_spinner);
+        adapter = ArrayAdapter.createFromResource(this,R.array.planets_spinner,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(),parent.getItemIdAtPosition(position)+" Selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
